c0c2a468fbea45af79d8ec6175a6e1b9f46c4017
cat <cat@TOM>
2016-08-13 23:38:10 +0800

    *)殿试内容配置

--- TomTeam.Project.Web/Controllers/PalaceController.cs
+++ TomTeam.Project.Web/Controllers/PalaceController.cs
@@ -1,17 +1,26 @@
-﻿using System;
+﻿using Abp.Application.Services.Dto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using TomTeam.Project.Gld;
 
 namespace TomTeam.Project.Web.Controllers
 {
     public class PalaceController : Controller
     {
-        // GET: Palace
-        public ActionResult Index()
+        private readonly IActivityConfigAppService _activityConfigAppService;
+        public PalaceController(IActivityConfigAppService _activityConfigAppService)
         {
-            return View();
+            this._activityConfigAppService = _activityConfigAppService;
+        }
+        // GET: Palace
+        public async Task<ActionResult> Index()
+        {
+            var model = await _activityConfigAppService.GetConfig(new NullableIdInput { Id = 0 });
+            return View(model);
         }
     }
 }
